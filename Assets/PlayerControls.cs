6fb899761ee3afefe6f624bc730c4e77b4555568
Aung Lin <nolin9889@gmail.com>
2020-06-21 20:54:08 -0700

    First commit

---
+++ Assets/PlayerControls.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour
+{
+    public KeyCode moveUp = KeyCode.W;
+    public KeyCode moveDown = KeyCode.S;
+    public float speed = 10.0f;
+    public float boundY = 2.25f;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        Debug.Log("Started PlayerControl");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rb2d == null)
+            Debug.LogWarning("Rigidbody not attached to player " + gameObject.name);
+
+        Debug.Log("Started PlayerControls Update");
+        var vel = rb2d.velocity;
+        if (Input.GetKey(moveUp)) {
+            Debug.Log("moveUp");
+
+            vel.y = speed;
+        }
+        else if (Input.GetKey(moveDown))
+        {
+            vel.y = -speed;
+        }
+        else 
+        {
+            vel.y = 0;
+        }
+        rb2d.velocity = vel;
+
+        var pos = transform.position;
+        if (pos.y > boundY)
+        {
+            pos.y = boundY;
+        }
+        else if (pos.y < -boundY)
+        {
+            pos.y = -boundY;
+        }
+        transform.position = pos;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+"Pong of the Dead" 
